5ddb6b248a7c1ade81a9cb29f155e3d453337e22
delbusque <del.busque@gmail.com>
2023-01-16 12:31:30 +0200

    Add Table Model

---
+++ backend/models/Table.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const tableSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    opened: {
+        type: Boolean,
+        required: true
+    },
+    orders: {
+        type: Array,
+        required: true
+    },
+    bill: {
+        type: Number,
+        required: true
+    },
+    capacity: {
+        type: Number
+    },
+    reserved: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('Table', tableSchema);
