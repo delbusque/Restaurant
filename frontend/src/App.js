86b434d2def92ef03066bd3b64a7992916b97955
Ventsislav Peychev <del.busque@gmail.com>
2023-01-18 00:37:01 +0200

    Add dummy Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './css/App.css';
 
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Tables from './components/Tables.js';
 
 import Navigation from './components/Navigation.js';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/tables' element={<Tables />} />
       </Routes>
 
