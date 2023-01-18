0e045f8babbfab56a306e691267fd9f41830a4d7
delbusque <del.busque@gmail.com>
2023-01-18 15:38:48 +0200

    Wrap li tags with Link

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -9,14 +9,16 @@ const Navigation = () => {
         <header className="header">
             <h1 className="deli"><Link className='links' to='/'>Deli</Link></h1>
             <ul role="list" className="nav">
-                <li className={location.pathname === '/tables' ? "nav__item-on" : "nav__item"}>
-                    <Link className='links' to='/tables'>Tables</Link>
-                </li>
-                <li className="nav__item">Items</li>
+                <Link className='links' to='/tables'>
+                    <li className={location.pathname === '/tables' ? "nav__item-on" : "nav__item"}>Tables</li>
+                </Link>
+                <Link className='links'>
+                    <li className="nav__item">Items</li>
+                </Link>
             </ul>
             <ul role="list" className="nav">
-                <li className="nav__item-auth">Login</li>
-                <li className="nav__item-auth">Register</li>
+                <Link className='links'><li className="nav__item-auth">Login</li></Link>
+                <Link className='links'><li className="nav__item-auth">Register</li></Link>
             </ul>
         </header>
     )
