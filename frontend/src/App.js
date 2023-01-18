b88db3dead85e956e0b74b8999d3798c7e5a40c0
delbusque <del.busque@gmail.com>
2023-01-18 15:38:06 +0200

    Lift state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import './css/App.css';
 
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
 import Home from './components/Home';
 import Tables from './components/Tables.js';
+import TableCard from './components/TableCard.js';
+
+import * as tablesService from './services/tablesService.js'
 
 import Navigation from './components/Navigation.js';
 
 function App() {
+
+  const [tables, setTables] = useState(null);
+
+  useEffect(() => {
+    tablesService.fetchTables(setTables);
+  }, [])
 
   return (
     <div className="App">
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/tables' element={<Tables />} />
+        <Route path='/tables' element={<Tables tables={tables} />} />
+        <Route path='/tables/:number' element={<TableCard tables={tables} />} />
       </Routes>
 
     </div>
--- frontend/src/components/Tables.js
+++ frontend/src/components/Tables.js
@@ -1,35 +1,20 @@
-import { useState, useEffect } from "react";
-import TableButton from "./TableButton.js";
+import { Link } from 'react-router-dom';
 
-const Tables = () => {
 
-    const [tables, setTables] = useState(null);
-
-    useEffect(() => {
-        async function fetchTables() {
-            try {
-                let responce = await fetch('/tables');
-                let json = await responce.json();
-                if (responce.ok) {
-                    setTables(json);
-                }
-            } catch (error) {
-                console.log(error.message);
-            }
-        }
-        fetchTables();
-    }, [])
-
+const Tables = ({ tables }) => {
 
     return (
         <section className="tables-cont">
             {tables && tables.map(t => (
-                <div className="table-btn" key={t._id}>
-                    <TableButton table={t} />
-                </div>
-            ))}
-            {!tables && <p className="error">No connection with database !</p>}
-        </section>
+                <Link to={t.number} className="links" key={t._id}>
+                    <div className="table-btn" >
+                        <p className="table-btn-text">{t.number}</p>
+                    </div>
+                </Link>
+            ))
+            }
+            {/* {!tables && <p className="error">No connection with database !</p>} */}
+        </section >
     )
 }
 
