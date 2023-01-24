13fe4fd075317eaeb21db99e1a3f0439a76bf094
delbusque <del.busque@gmail.com>
2023-01-24 12:13:14 +0200

    Add items context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,40 @@ import './css/App.css';
 import { Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from "react";
 
+import ItemsContext from './contexts/ItemsContext.js';
+
 import Home from './components/Home';
 import Tables from './components/Tables.js';
-import TableCard from './components/TableCard.js';
+import TableView from './components/TableView.js';
 
-import * as tablesService from './services/tablesService.js'
+import * as apiService from './services/apiService.js'
 
 import Navigation from './components/Navigation.js';
 
 function App() {
 
   const [tables, setTables] = useState(null);
-  // const [items, setItems] = useState(null);
+  const [items, setItems] = useState(null);
 
   useEffect(() => {
-    tablesService.fetchTables(setTables);
+    apiService.fetchTables(setTables);
+  }, [])
+
+  useEffect(() => {
+    apiService.fetchItems(setItems);
   }, [])
 
   return (
     <div className="App">
       <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/tables' element={<Tables tables={tables} />} />
-        <Route path='/tables/:number' element={<TableCard tables={tables} setTables={setTables} />} />
-      </Routes>
+
+      <ItemsContext.Provider value={{ items }}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/tables' element={<Tables tables={tables} />} />
+          <Route path='/tables/:number' element={<TableView tables={tables} setTables={setTables} />} />
+        </Routes>
+      </ItemsContext.Provider>
 
     </div>
   );
