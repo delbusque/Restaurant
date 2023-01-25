7c4cb1b56a069740e4e20aaa1cae5543739c507e
delbusque <del.busque@gmail.com>
2023-01-25 11:01:24 +0200

    Move family handler to child

--- frontend/src/components/FamilyButton.js
+++ frontend/src/components/FamilyButton.js
@@ -1,6 +1,20 @@
-const FamilyButton = ({ family, drinkIsActive, foodIsAcitve }) => {
+const FamilyButton = ({ family, setFoodIsActive, setDrinkIsActive, setTypeIsActive }) => {
+
+    const familyHandler = (e) => {
+        if (e.target.name === 'drinks') {
+            setDrinkIsActive(true);
+            setFoodIsActive(false);
+            setTypeIsActive(false);
+        } else if (e.target.name === 'food') {
+            setFoodIsActive(true);
+            setDrinkIsActive(false);
+            setTypeIsActive(false);
+        }
+    }
+
     return (
-        <button className={family === 'drinks' ? 'family-btn-drinks' : 'family-btn-food'} name={family}>{family.toUpperCase()}</button>
+        <button className={family === 'drinks' ? 'family-btn-drinks' : 'family-btn-food'}
+            onClick={(e) => familyHandler(e)} name={family}>{family.toUpperCase()}</button>
     )
 }
 
