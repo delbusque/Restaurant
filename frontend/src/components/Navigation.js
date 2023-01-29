623e64319fe286d2271218c0d7a7ecebb87af7f7
delbusque <del.busque@gmail.com>
2023-01-29 11:39:35 +0200

    Update Navigation.js

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,23 +1,21 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Navigation = () => {
 
-    const location = useLocation();
-
     return (
         <header className="header">
             <h1 className="deli"><Link className='links' to='/'>Deli</Link></h1>
-            <ul role="list" className="nav">
+            <ul className="nav">
                 <Link className='links' to='/tables'>
                     <li className="nav__item">Tables</li>
                     {/* <li className={location.pathname === '/tables' ? "nav__item-on" : "nav__item"}>Tables</li> */}
                 </Link>
-                <Link className='links'>
+                <Link className='links' to='/items'>
                     <li className="nav__item">Items</li>
                 </Link>
             </ul>
-            <ul role="list" className="nav">
+            <ul className="nav">
                 <Link className='links'><li className="nav__item-auth">Login</li></Link>
                 <Link className='links'><li className="nav__item-auth">Register</li></Link>
             </ul>
