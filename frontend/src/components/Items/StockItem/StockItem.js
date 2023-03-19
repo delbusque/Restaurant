ac36b0fda5cc5bad888173d5d031dd87740016fa
Ventsislav Peychev <del.busque@gmail.com>
2023-03-19 16:46:40 +0200

    Add delete stock item handler

--- frontend/src/components/Items/StockItem/StockItem.js
+++ frontend/src/components/Items/StockItem/StockItem.js
@@ -1,6 +1,18 @@
 import styles from './StockItem.module.css'
 
 const StockItem = ({ item }) => {
+
+    const deleteHandler = async () => {
+        console.log(item._id);
+        const responce = await fetch('/items/' + item._id, {
+            method: 'DELETE',
+        });
+
+        const result = await responce.json();
+        console.log(result);
+
+    }
+
     return (
         <div className={styles['stock-item']}>
             <div className={styles['stock-item__name']}>{item.name}</div>
@@ -13,7 +25,7 @@ const StockItem = ({ item }) => {
             <button className={styles['stock-item__edit']}>
                 <i className="fa-solid fa-marker marker"></i>
             </button>
-            <button className={styles['stock-item__delete']}>
+            <button className={styles['stock-item__delete']} onClick={deleteHandler}>
                 <i className="fa-solid fa-trash-arrow-up trash"></i>
             </button>
 
