764e998ffdb3cf1c9f2b0a58e8f3ecbd643deec3
Ventsislav Peychev <del.busque@gmail.com>
2023-03-19 15:18:37 +0200

    Add edit and delete buttons at stock item

--- frontend/src/components/Items/StockItem/StockItem.js
+++ frontend/src/components/Items/StockItem/StockItem.js
@@ -2,14 +2,19 @@ import styles from './StockItem.module.css'
 
 const StockItem = ({ item, addItemHandler }) => {
     return (
-        <div className={styles['stock-item']} onClick={() => addItemHandler(item)}>
+        <div className={styles['stock-item']}>
             <div className={styles['stock-item__name']}>{item.name}</div>
-
             <div className={styles['stock-item__quantity']}>{item.quantity.toFixed(3)}
                 <span className={styles['stock-item__quantityType']}>{item.quantityType}</span>
             </div>
+            <div className={styles['stock-item__price']}>{item.price.toFixed(2)}
+                <span className='lv'> лв.</span></div>
 
-            <div className={styles['stock-item__price']}>{item.price.toFixed(2)}<span className='lv'> лв.</span></div>
+            <button className={styles['stock-item__edit']}><i class="fa-solid fa-marker marker"></i></button>
+            <button className={styles['stock-item__delete']}><i class="fa-solid fa-trash-arrow-up trash"></i></button>
+            {/* <button className={styles['button-53-green']} role="button" >Edit</button> */}
+            {/* <button className={styles['button-53']} role="button" >Del</i></button> */}
+
         </div>
     )
 }
