7ccd228014a86293d414c661269168683f003399
Ventsislav Peychev <del.busque@gmail.com>
2023-03-22 16:35:40 +0200

    Add signup user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,14 @@ const loginUser = async (req, res) => {
 }
 
 const signupUser = async (req, res) => {
-    res.status(200).json({ mssg: 'user signupUser' })
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {
