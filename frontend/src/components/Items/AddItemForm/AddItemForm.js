0b872548b282e3a68cd876a540be8bcd527018cb
Ventsislav Peychev <del.busque@gmail.com>
2023-03-23 11:04:41 +0200

    Show error on empty form fields

--- frontend/src/components/Items/AddItemForm/AddItemForm.js
+++ frontend/src/components/Items/AddItemForm/AddItemForm.js
@@ -22,99 +22,105 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
         e.preventDefault();
         const newItem = { name, family, price, type, quantity };
 
-        if (name && family && type && price && quantity && !items.find(i => i.name === name)) {
+        // if (name && family && type && price && quantity && !items.find(i => i.name === name)) {
 
-            const response = await fetch('/items/add', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newItem)
-            })
+        const response = await fetch('/items/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newItem)
+        })
 
-            const result = await response.json();
+        const result = await response.json();
 
-            if (!response.ok) {
-                setError(result.error)
-            }
-            if (response.ok) {
-                setItems(oldState => [...oldState, result]);
-                window.localStorage.setItem('items', JSON.stringify(items));
-                setError(null)
-                setName('')
-                setPrice('')
-                setQuantity('')
-                if (newItem.family === 'drinks') {
-                    setDrinkIsActive(true);
-                    setFoodIsActive(false);
-                } else if (newItem.family === 'food') {
-                    setDrinkIsActive(false);
-                    setFoodIsActive(true);
-                }
+        if (!response.ok) {
+            setError(result.error)
+        }
+        if (response.ok) {
+            setItems(oldState => [...oldState, result]);
+            window.localStorage.setItem('items', JSON.stringify(items));
+            setError(null)
+            setName('')
+            setPrice('')
+            setQuantity('')
+            if (newItem.family === 'drinks') {
+                setDrinkIsActive(true);
+                setFoodIsActive(false);
+            } else if (newItem.family === 'food') {
+                setDrinkIsActive(false);
+                setFoodIsActive(true);
             }
         }
+        // }
     }
 
     return (
-        <form id={styles['msform']} onSubmit={addNewStockItemHandler}>
-            <fieldset>
-                <h2 className={styles['fs-title']}>Add new item</h2>
-                {/* <h3 className="fs-subtitle">We will never sell it</h3> */}
-                <input type="text" name="name" placeholder="Name"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                />
+        <>
 
-                <select id={styles['select-family']}
-                    onChange={(e) => {
-                        setFamily(e.target.value)
-                        setType('');
-                    }}>
-                    <option selected disabled>Choose here :</option>
-                    <option value='drinks'>Drinks</option>
-                    <option value='food'>Food</option>
-                </select>
+            <form id={styles['msform']} onSubmit={addNewStockItemHandler}>
+                <fieldset>
+                    <h2 className={styles['fs-title']}>Add new item</h2>
+                    {/* <h3 className="fs-subtitle">We will never sell it</h3> */}
+                    <input type="text" name="name" placeholder="Name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
 
-                {family === 'drinks' &&
-                    <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
-                        <option selected disabled>Choose from drinks :</option>
-                        <option value='beer'>Beer</option>
-                        <option value='vodka'>Vodka</option>
-                        <option value='wine'>Wine</option>
-                        <option value='whiskey'>Whiskey</option>
-                        <option value='gin'>Gin</option>
-                        <option value='mastika'>Mastika</option>
-                        <option value='rum'>Rum</option>
-                        <option value='juice'>Juice</option>
-                        <option value='fresh'>Fresh</option>
-                        <option value='water'>Water</option>
-                        <option value='other'>Other</option>
-                    </select>}
+                    <select id={styles['select-family']}
+                        onChange={(e) => {
+                            setFamily(e.target.value)
+                            setType('');
+                        }}>
+                        <option selected disabled>Choose here :</option>
+                        <option value='drinks'>Drinks</option>
+                        <option value='food'>Food</option>
+                    </select>
 
-                {family === 'food' &&
-                    <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
-                        <option selected disabled>Choose from food :</option>
-                        <option value='salad'>Salad</option>
-                        <option value='grill'>Grill</option>
-                    </select>}
+                    {family === 'drinks' &&
+                        <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
+                            <option selected disabled>Choose from drinks :</option>
+                            <option value='beer'>Beer</option>
+                            <option value='vodka'>Vodka</option>
+                            <option value='wine'>Wine</option>
+                            <option value='whiskey'>Whiskey</option>
+                            <option value='gin'>Gin</option>
+                            <option value='mastika'>Mastika</option>
+                            <option value='rum'>Rum</option>
+                            <option value='juice'>Juice</option>
+                            <option value='fresh'>Fresh</option>
+                            <option value='water'>Water</option>
+                            <option value='other'>Other</option>
+                        </select>}
 
-                <input type="text" name="price" placeholder="Price"
-                    onChange={(e) => setPrice(e.target.value)}
-                    value={price}
-                />
-                <input type="text" name="quantity" placeholder="Quantity"
-                    onChange={(e) => setQuantity(e.target.value)}
-                    value={quantity}
+                    {family === 'food' &&
+                        <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
+                            <option selected disabled>Choose from food :</option>
+                            <option value='salad'>Salad</option>
+                            <option value='grill'>Grill</option>
+                        </select>}
 
-                />
-                <input
-                    type="submit"
-                    className={styles['action-button']}
-                    defaultValue="Previous"
-                />
+                    <input type="text" name="price" placeholder="Price"
+                        onChange={(e) => setPrice(e.target.value)}
+                        value={price}
+                    />
+                    <input type="text" name="quantity" placeholder="Quantity"
+                        onChange={(e) => setQuantity(e.target.value)}
+                        value={quantity}
 
-            </fieldset>
-        </form>
+                    />
+                    <input
+                        type="submit"
+                        className={styles['action-button']}
+                        defaultValue="Previous"
+                    />
+
+
+                </fieldset>
+            </form>
+            {error && <div className={styles['error']}>{error}</div>}
+
+        </>
     )
 }
 
