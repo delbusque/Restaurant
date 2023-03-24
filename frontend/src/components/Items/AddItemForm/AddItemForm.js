3d48adcb8f3d10b9484078422c6de45048695f14
Ventsislav Peychev <del.busque@gmail.com>
2023-03-24 11:54:11 +0200

    Handle duplicated names with different lower and upper case letter combinations. Make capitalization of first item letter

--- frontend/src/components/Items/AddItemForm/AddItemForm.js
+++ frontend/src/components/Items/AddItemForm/AddItemForm.js
@@ -7,12 +7,13 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
 
     const { items, setItems } = useContext(ItemsContext);
 
-    const [name, setName] = useState('');
+    const [inputName, setInputName] = useState('');
     const [family, setFamily] = useState('');
     const [type, setType] = useState('');
     const [price, setPrice] = useState('');
     const [quantity, setQuantity] = useState('');
     const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const typeHandler = (e) => {
         setType(e.target.value);
@@ -20,9 +21,9 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
 
     const addNewStockItemHandler = async (e) => {
         e.preventDefault();
-        const newItem = { name, family, price, type, quantity };
+        let name = inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
 
-        // if (name && family && type && price && quantity && !items.find(i => i.name === name)) {
+        const newItem = { name, family, price, type, quantity }
 
         const response = await fetch('/items/add', {
             method: 'POST',
@@ -34,16 +35,22 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
 
         const result = await response.json();
 
-        if (!response.ok) {
-            setError(result.error)
+        if (!response.ok && result.emptyFields) {
+            setError(result.error);
+            setEmptyFields(result.emptyFields);
+        }
+        if (!response.ok && result.error.includes('duplicate key')) {
+            setError(`${name} is already in stock !`);
+            setEmptyFields(['name']);
         }
         if (response.ok) {
             setItems(oldState => [...oldState, result]);
             window.localStorage.setItem('items', JSON.stringify(items));
-            setError(null)
-            setName('')
-            setPrice('')
-            setQuantity('')
+            setError(null);
+            setInputName('');
+            setPrice('');
+            setQuantity('');
+            setEmptyFields([]);
             if (newItem.family === 'drinks') {
                 setDrinkIsActive(true);
                 setFoodIsActive(false);
@@ -52,22 +59,23 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
                 setFoodIsActive(true);
             }
         }
-        // }
     }
 
     return (
         <>
 
-            <form id={styles['msform']} onSubmit={addNewStockItemHandler}>
+            <form className={styles['msform']} onSubmit={addNewStockItemHandler}>
                 <fieldset>
                     <h2 className={styles['fs-title']}>Add new item</h2>
                     {/* <h3 className="fs-subtitle">We will never sell it</h3> */}
-                    <input type="text" name="name" placeholder="Name"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
+                    <input className={emptyFields.includes('name') ? styles['input-error'] : ''}
+                        type="text" name="name" placeholder="Name"
+                        onChange={(e) => setInputName(e.target.value)}
+                        value={inputName}
                     />
 
-                    <select id={styles['select-family']}
+                    <select
+                        className={emptyFields.includes('family') ? styles['input-error'] : ''}
                         onChange={(e) => {
                             setFamily(e.target.value)
                             setType('');
@@ -78,7 +86,8 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
                     </select>
 
                     {family === 'drinks' &&
-                        <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
+                        <select onChange={(e) => typeHandler(e)}
+                            className={emptyFields.includes('type') ? styles['input-error'] : ''}>
                             <option selected disabled>Choose from drinks :</option>
                             <option value='beer'>Beer</option>
                             <option value='vodka'>Vodka</option>
@@ -98,17 +107,20 @@ const AddItemForm = ({ setDrinkIsActive, setFoodIsActive }) => {
                         </select>}
 
                     {family === 'food' &&
-                        <select id={styles['select-type']} onChange={(e) => typeHandler(e)}>
+                        <select onChange={(e) => typeHandler(e)}
+                            className={emptyFields.includes('type') ? styles['input-error'] : ''}>
                             <option selected disabled>Choose from food :</option>
                             <option value='salad'>Salad</option>
                             <option value='grill'>Grill</option>
                         </select>}
 
-                    <input type="text" name="price" placeholder="Price"
+                    <input type="number" name="price" placeholder="Price"
+                        className={emptyFields.includes('price') ? styles['input-error'] : ''}
                         onChange={(e) => setPrice(e.target.value)}
                         value={price}
                     />
-                    <input type="text" name="quantity" placeholder="Quantity"
+                    <input type="number" name="quantity" placeholder="Quantity"
+                        className={emptyFields.includes('quantity') ? styles['input-error'] : ''}
                         onChange={(e) => setQuantity(e.target.value)}
                         value={quantity}
 
