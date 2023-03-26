45f76a6a30b2190a206d4ca0ac19400314892a4b
Ventsislav Peychev <del.busque@gmail.com>
2023-03-26 12:27:56 +0300

    Add login and signup User methods with validations

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,14 @@ const userSchema = new Schema({
 
 userSchema.statics.signup = async function (email, password) {
 
+    if (!email || !password) {
+        throw Error('All fields must be filled !')
+    }
+
+    if (!validator.isEmail(email) || !validator.isStrongPassword(password)) {
+        throw Error('Invalid credentials');
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
@@ -27,6 +36,27 @@ userSchema.statics.signup = async function (email, password) {
     const hash = await bcrypt.hash(password, salt);
 
     const user = await this.create({ email, password: hash });
+    return user;
+}
+
+userSchema.statics.login = async function (email, password) {
+
+    if (!email || !password) {
+        throw Error('All login fields must be filled')
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error('Login credentials do not match !')
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error('Login credentials do not match !')
+    }
+
     return user;
 }
 
