aeb8a137dc008ba51b258b6a1cb259de05d69841
Ventsislav Peychev <del.busque@gmail.com>
2023-03-31 17:46:47 +0300

    Navigation behaviour upon user

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,35 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navigation = () => {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
+    const navigate = useNavigate();
+
     const logoutHandler = () => {
         logout();
+        navigate('/');
     }
 
     return (
         <header className="header">
             <h1 className="deli"><Link className='links' to='/'>Deli</Link></h1>
             <ul className="nav">
-                <Link className='links' to='/tables'>
+                {user && <Link className='links' to='/tables'>
                     <li className="nav__item">Tables</li>
-                    {/* <li className={location.pathname === '/tables' ? "nav__item-on" : "nav__item"}>Tables</li> */}
-                </Link>
+                </Link>}
                 <Link className='links' to='/items'>
                     <li className="nav__item">Items</li>
                 </Link>
             </ul>
 
-            <ul className="nav">
-                <div className="nav__auth">
-                    <button className="nav__item-auth" onClick={logoutHandler}>Logout</button>
-                </div>
 
-                <div className="nav__auth">
-                    <Link className='links' to='/login'><li className="nav__item-auth">Login</li></Link>
-                    <Link className='links' to='/signup'><li className="nav__item-auth">Sign Up</li></Link>
-                </div>
-            </ul>
+            {user && (<div className="nav__auth">
+                <span className='nav__auth__email'>{user.email}</span>
+                <button className="nav__item-auth" onClick={logoutHandler}>Logout</button>
+            </div>)}
+
+            {!user && (<div className="nav__auth">
+                <Link className='links' to='/login'><li className="nav__item-auth">Login</li></Link>
+                <Link className='links' to='/signup'><li className="nav__item-auth">Sign Up</li></Link>
+            </div>)}
+
         </header>
     )
 }
