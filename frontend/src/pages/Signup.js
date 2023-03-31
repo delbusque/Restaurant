be54350cd467ff03744e4dceb66e361afd08c97a
Ventsislav Peychev <del.busque@gmail.com>
2023-03-31 11:05:14 +0300

    Add signup with localStorage and authContext user persisting

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,21 +1,30 @@
 import styles from './Signup.module.css'
 
 import { useState } from "react";
+import { useSignup } from '../hooks/useSignup';
 
 const Signup = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
+    const [checkPasswords, setCheckPasswords] = useState('');
 
-    const signHandler = (e) => {
+    const { signup, error, setError, isLoading } = useSignup();
+
+    const signupHandler = async (e) => {
         e.preventDefault();
 
-        console.log(email, password, repeatPassword);
+        if (password !== repeatPassword) {
+            setError('Passwords do not match !');
+            return null;
+        }
+
+        await signup(email, password);
     }
 
     return (
-        <form className={styles['signup']} onSubmit={signHandler}>
+        <form className={styles['signup']} onSubmit={signupHandler}>
             <h3>Sign Up</h3>
 
             <label>Email:</label>
@@ -33,8 +42,8 @@ const Signup = () => {
                 onChange={(e) => setRepeatPassword(e.target.value)}
                 value={repeatPassword} />
 
-            <button>Sign Up</button>
-
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className={styles['error']}>{error}</div>}
         </form >
     )
 }
