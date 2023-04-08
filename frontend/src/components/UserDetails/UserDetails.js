0e0c727cb1ffef45be12003519e1525da95c44c2
Ventsislav Peychev <del.busque@gmail.com>
2023-04-08 16:56:52 +0300

    Add User details component

---
+++ frontend/src/components/UserDetails/UserDetails.js
@@ -0,0 +1,20 @@
+const UserDetails = ({ user }) => {
+    return (
+        <div className="user-details">
+            {user.firstName || user.lastName
+                ? <h4>{user.firstName} {user.lastName}</h4>
+                : <h4>{user.email}</h4>
+            }
+            {
+                user.phone
+                    ? <p><strong><i class="fa-solid fa-phone-volume"></i> </strong>{user.phone}</p>
+                    : null
+            }
+
+            <span className="material-symbols-outlined" href={`mailto:${user.email}`}>
+                <a className='anchor-mail' href={`mailto:${user.email}`}>email</a></span>
+        </div>
+    )
+}
+
+export default UserDetails;
