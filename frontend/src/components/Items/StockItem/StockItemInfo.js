267c0719595b9064b9ff8de85c4cc0439683e522
Ventsislav Peychev <del.busque@gmail.com>
2023-04-15 15:59:21 +0300

    Add Item details view

--- frontend/src/components/Items/StockItem/StockItemInfo.js
+++ frontend/src/components/Items/StockItem/StockItemInfo.js
@@ -4,13 +4,15 @@ const StockItemInfo = ({ item, setShowInfo }) => {
     return (
         <div className={styles['msform']}>
             <fieldset>
-                <button className={styles['modal-close']} onClick={() => setShowInfo(false)}> X </button>
-                <h2 className={styles['fs-title']}>{item.name}</h2>
-
+                <button className={styles['modal-close']} onClick={() => setShowInfo(false)}> x </button>
+                <h2 className={styles['details-name']}>{item.name}</h2>
+                <div className={styles['details-family']}>{item.family}</div>
+                <div className={styles['details-type']}>{item.type}</div>
+                <div className={styles['details-quantity']}>{item.quantity.toFixed(3)} {item.quantityType}</div>
+                <div className={styles['details-price']}>{item.price.toFixed(2)} lv</div>
             </fieldset>
         </div>
     )
-
 }
 
 export default StockItemInfo;
