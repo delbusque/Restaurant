6f75caf6862523dfe840d6a9d345b4fe3f7755ec
Ventsislav Peychev <del.busque@gmail.com>
2023-04-15 10:00:27 +0300

    Add table error message upon requesting tables without authorization

--- frontend/src/components/Tables/TableError.js
+++ frontend/src/components/Tables/TableError.js
@@ -1,6 +1,9 @@
+import styles from './TableError.module.css'
+import { Link } from 'react-router-dom';
+
 const TableError = () => {
     return (
-        <div className="table-error">You are not authorized and should log in to proceed !</div>
+        <div className="table-error">You are not authorized and must <Link to='/login' className={styles['err-login']}> login</Link> or <Link to='/signup' className={styles['err-signup']}> sign up</Link> to proceed with Tables !</div>
     )
 }
 
--- frontend/src/components/Tables/Tables.js
+++ frontend/src/components/Tables/Tables.js
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom';
 
+import { useAuthContext } from "../../hooks/useAuthContext";
+import TableError from './TableError';
+
 const Tables = ({ tables }) => {
 
+    const { user } = useAuthContext();
+
     return (
-        <section className="tables-cont">
-            {tables && tables.map(t => (
-                <Link to={t.number} className="links" key={t._id}>
-                    {t.paid && <div className={t.opened ? "table-btn-opened" : "table-btn"} >
-                        <p className="table-btn-text">{t.number}</p>
-                    </div>}
-                    {!t.paid && <div className={t.opened ? "table-btn-unpaid" : "table-btn"} >
-                        <p className="table-btn-text">{t.number}</p>
-                    </div>}
-                </Link>
-            ))
+        <>
+            {user &&
+                <section className="tables-cont">
+                    {tables && tables.map(t => (
+                        <Link to={user ? t.number : null} className="links" key={t._id}>
+                            {t.paid && <div className={t.opened ? "table-btn-opened" : "table-btn"} >
+                                <p className="table-btn-text">{t.number}</p>
+                            </div>}
+                            {!t.paid && <div className={t.opened ? "table-btn-unpaid" : "table-btn"} >
+                                <p className="table-btn-text">{t.number}</p>
+                            </div>}
+                        </Link>
+                    ))
+                    }
+                </section >
             }
-        </section >
+            {!user && <TableError />}
+        </>
     )
 }
 
