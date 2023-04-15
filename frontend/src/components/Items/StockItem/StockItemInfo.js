ce623a84053a7a6b7485e45d8dd7ab00985753b6
Ventsislav Peychev <del.busque@gmail.com>
2023-04-15 17:28:01 +0300

    Fix issue with dynamic data

--- frontend/src/components/Items/StockItem/StockItemInfo.js
+++ frontend/src/components/Items/StockItem/StockItemInfo.js
@@ -5,11 +5,11 @@ const StockItemInfo = ({ item, setShowInfo }) => {
         <div className={styles['msform']}>
             <fieldset>
                 <button className={styles['modal-close']} onClick={() => setShowInfo(false)}> x </button>
-                <h2 className={styles['details-name']}>{item.name}</h2>
-                <div className={styles['details-family']}>{item.family}</div>
-                <div className={styles['details-type']}>{item.type}</div>
-                <div className={styles['details-quantity']}>{item.quantity.toFixed(3)} {item.quantityType}</div>
-                <div className={styles['details-price']}>{item.price.toFixed(2)} lv</div>
+                <h2 className={styles['details-name']}>{item && item.name}</h2>
+                <div className={styles['details-family']}>{item && item.family}</div>
+                <div className={styles['details-type']}>{item && item.type}</div>
+                <div className={styles['details-quantity']}>{item && item.quantity.toFixed(3)} {item && item.quantityType}</div>
+                <div className={styles['details-price']}>{item && item.price.toFixed(2)} lv</div>
             </fieldset>
         </div>
     )
