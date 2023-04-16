dc4946de9e848a3c4836f8ba8432c1220317bf70
Ventsislav Peychev <del.busque@gmail.com>
2023-04-16 16:05:22 +0300

    Change html attributes

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -9,19 +9,18 @@ const Account = () => {
     const [emptyFields, setEmptyFields] = useState([]);
     const [error, setError] = useState(null);
 
-
     return (
         <div className={styles['acc']}>
 
             <div className={styles['acc-details']}>
                 <div className={styles['acc__name']}>
-                    <div className={styles['acc__name-icon']}><i class="fa-regular fa-user"></i></div>
+                    <div className={styles['acc__name-icon']}><i className="fa-regular fa-user"></i></div>
                     <div className={styles['acc__name-label']}>Name:</div>
                     <div className={styles['acc__name-name']}>Curtis Nathan</div>
                 </div>
 
                 <div className={styles['acc__phone']}>
-                    <div className={styles['acc__name-icon']}><i class="fa-solid fa-mobile-retro"></i></div>
+                    <div className={styles['acc__name-icon']}><i className="fa-solid fa-mobile-retro"></i></div>
                     <div className={styles['acc__name-label']}>Phone:</div>
                     <div className={styles['acc__name-name']}>+359 88 1234567</div>
                 </div>
@@ -34,24 +33,23 @@ const Account = () => {
                         <h2 className={styles['fs-title']}>Edit your details</h2>
                         {/* <h3 className="fs-subtitle">We will never sell it</h3> */}
                         <input className={emptyFields.includes('firstName') ? styles['input-error'] : ''}
-                            type="text" name="name" placeholder="First Name"
+                            type="text" name="firstName" placeholder="First Name"
                             onChange={(e) => setFirstName(e.target.value)}
                             value={firstName}
                         />
                         <input className={emptyFields.includes('lastName') ? styles['input-error'] : ''}
-                            type="text" name="name" placeholder="Last Name"
+                            type="text" name="lastName" placeholder="Last Name"
                             onChange={(e) => setLastName(e.target.value)}
                             value={lastName}
                         />
                         <input className={emptyFields.includes('phone') ? styles['input-error'] : ''}
-                            type="text" name="name" placeholder="Phone number"
+                            type="text" name="phone" placeholder="Phone number"
                             onChange={(e) => setPhone(e.target.value)}
                             value={phone}
                         />
                         <input
                             type="submit"
                             className={styles['action-button']}
-                            defaultValue="Previous"
                             value='Edit'
                         />
                     </fieldset>
