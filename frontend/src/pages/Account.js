f85e52918000064e395850ad065eb0955b6d21c6
Ventsislav Peychev <del.busque@gmail.com>
2023-04-17 15:33:05 +0300

    Add form request and styles

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,50 +1,69 @@
 import styles from './Account.module.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useEditUser } from '../hooks/useEditUser';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Account = () => {
+
+    const { user } = useAuthContext();
+
+    const { editUser, isLoading, error, setError, emptyFields, setEmptyFields } = useEditUser();
 
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [phone, setPhone] = useState('');
-    const [emptyFields, setEmptyFields] = useState([]);
-    const [error, setError] = useState(null);
+
+    const editUserHandler = async (e) => {
+        e.preventDefault();
+
+        await editUser(user.email, firstName, lastName, phone);
+        setFirstName('');
+        setLastName('');
+        setPhone('');
+    }
 
     return (
         <div className={styles['acc']}>
-
             <div className={styles['acc-details']}>
                 <div className={styles['acc__name']}>
                     <div className={styles['acc__name-icon']}><i className="fa-regular fa-user"></i></div>
                     <div className={styles['acc__name-label']}>Name:</div>
-                    <div className={styles['acc__name-name']}>Curtis Nathan</div>
+                    <div className={styles['acc__name-name']}>{user.firstName} {user.lastName}</div>
                 </div>
 
                 <div className={styles['acc__phone']}>
                     <div className={styles['acc__name-icon']}><i className="fa-solid fa-mobile-retro"></i></div>
                     <div className={styles['acc__name-label']}>Phone:</div>
-                    <div className={styles['acc__name-name']}>+359 88 1234567</div>
+                    <div className={styles['acc__name-name']}>{user.phone}</div>
                 </div>
             </div>
 
             <div className={styles['acc-form']}>
-
-                <form className={styles['msform']}>
+                <form className={styles['msform']} onSubmit={editUserHandler}>
                     <fieldset>
                         <h2 className={styles['fs-title']}>Edit your details</h2>
-                        {/* <h3 className="fs-subtitle">We will never sell it</h3> */}
                         <input className={emptyFields.includes('firstName') ? styles['input-error'] : ''}
                             type="text" name="firstName" placeholder="First Name"
-                            onChange={(e) => setFirstName(e.target.value)}
+                            onChange={(e) => {
+                                setFirstName(e.target.value)
+                                setEmptyFields(old => old.filter(f => f !== 'firstName'));
+                            }}
                             value={firstName}
                         />
                         <input className={emptyFields.includes('lastName') ? styles['input-error'] : ''}
                             type="text" name="lastName" placeholder="Last Name"
-                            onChange={(e) => setLastName(e.target.value)}
+                            onChange={(e) => {
+                                setLastName(e.target.value)
+                                setEmptyFields(old => old.filter(f => f !== 'lastName'));
+                            }}
                             value={lastName}
                         />
                         <input className={emptyFields.includes('phone') ? styles['input-error'] : ''}
                             type="text" name="phone" placeholder="Phone number"
-                            onChange={(e) => setPhone(e.target.value)}
+                            onChange={(e) => {
+                                setPhone(e.target.value)
+                                setEmptyFields(old => old.filter(f => f !== 'phone'));
+                            }}
                             value={phone}
                         />
                         <input
