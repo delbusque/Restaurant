f1d8f34e509ceb7265a723d982614dda164a2227
Ventsislav Peychev <del.busque@gmail.com>
2023-04-17 10:22:45 +0300

    Remove unused controller functions

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -5,28 +5,6 @@ const getAllTables = async (req, res) => {
     res.status(200).json(tables);
 }
 
-const getSingleTable = async (req, res) => {
-    const name = req.params.name
-    const table = await Table.findOne({ name });
-
-    if (!table) {
-        return res.status(404).json({ error: 'No such table' });
-    }
-    res.status(200).json(table);
-}
-
-const getOpenedTables = async (req, res) => {
-    const openedTables = await Table.find({ opened: true });
-
-    if (openedTables.length < 1) {
-        return res.json({ mssg: 'No opened tables' });
-    }
-    res.status(200).json(openedTables);
-}
-
-
 module.exports = {
-    getAllTables,
-    getSingleTable,
-    getOpenedTables
+    getAllTables
 }
