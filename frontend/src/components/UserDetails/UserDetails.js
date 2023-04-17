8229cb45bb551573e8d67c3c9436116624ca3b7c
Ventsislav Peychev <del.busque@gmail.com>
2023-04-17 16:53:52 +0300

    Add direct phone call on user`s phone numbers

--- frontend/src/components/UserDetails/UserDetails.js
+++ frontend/src/components/UserDetails/UserDetails.js
@@ -1,3 +1,5 @@
+import styles from './UserDetails.module.css';
+
 const UserDetails = ({ user }) => {
     return (
         <div className="user-details">
@@ -7,7 +9,8 @@ const UserDetails = ({ user }) => {
             }
             {
                 user.phone
-                    ? <p><strong><i className="fa-solid fa-phone-volume"></i> </strong>{user.phone}</p>
+                    ? <p><strong><i className="fa-solid fa-phone-volume"></i> </strong>
+                        <a className={styles['phone-link']} href={`tel:${user.phone}`}>{user.phone}</a></p>
                     : null
             }
 
