e53821145563998283b28020f6c48cadca540fe2
Ventsislav Peychev <del.busque@gmail.com>
2023-04-18 13:36:58 +0300

    Add own messages to Account page

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -77,6 +77,92 @@ const Account = () => {
 
             </div>
 
+            <div className={styles['posts-cont']}>
+                <div className={styles["blog-list-row"]}>
+                    <div className={styles["left-info"]}>
+                        <div className={styles["author-cont"]}>
+                            <div className={styles["post-author-name"]}>by {user.firstName} {user.lastName}</div>
+                            {user &&
+                                <div className={styles["post-icons"]}>
+                                    <button className={styles['edit']} >
+                                        <i className="fa-solid fa-marker marker"></i>
+                                    </button>
+                                    <button className={styles['delete']} >
+                                        <i className="fa-solid fa-trash-arrow-up trash"></i>
+                                    </button>
+                                </div>
+                            }
+                        </div>
+                        <div className={styles["date-published"]}>August 12, 2016</div>
+                    </div>
+
+                    <div className={styles["post-text"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lectus
+                        massa. Pellentesque ornare mauris a auctor pellentesque. Sed sit amet
+                        metus at odio venenatis elemen mauris. Fusce mauris
+                        nibh, gravida eu eros sed, suscipit sollicitudin dui. Etiam tellus
+                        justo, fringilla at tempor in, mollis et felis.{" "}
+                    </div>
+
+                </div>
+
+                <div className={styles["blog-list-row"]}>
+                    <div className={styles["left-info"]}>
+                        <div className={styles["author-cont"]}>
+                            <div className={styles["post-author-name"]}>by {user.firstName} {user.lastName}</div>
+                            {user &&
+                                <div className={styles["post-icons"]}>
+                                    <button className={styles['edit']} >
+                                        <i className="fa-solid fa-marker marker"></i>
+                                    </button>
+                                    <button className={styles['delete']} >
+                                        <i className="fa-solid fa-trash-arrow-up trash"></i>
+                                    </button>
+                                </div>
+                            }
+                        </div>
+                        <div className={styles["date-published"]}>August 12, 2016</div>
+                    </div>
+
+                    <div className={styles["post-text"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lectus
+                        massa. Pellentesque ornare mauris a auctor pellentesque. Sed sit amet
+                        metus at odio venenatis elemen mauris. Fusce mauris
+                        nibh, gravida eu eros sed, suscipit sollicitudin dui. Etiam tellus
+                        justo, fringilla at tempor in, mollis et felis.{" "}
+                    </div>
+
+                </div>
+
+                <div className={styles["blog-list-row"]}>
+                    <div className={styles["left-info"]}>
+                        <div className={styles["author-cont"]}>
+                            <div className={styles["post-author-name"]}>by {user.firstName} {user.lastName}</div>
+                            {user &&
+                                <div className={styles["post-icons"]}>
+                                    <button className={styles['edit']} >
+                                        <i className="fa-solid fa-marker marker"></i>
+                                    </button>
+                                    <button className={styles['delete']} >
+                                        <i className="fa-solid fa-trash-arrow-up trash"></i>
+                                    </button>
+                                </div>
+                            }
+                        </div>
+                        <div className={styles["date-published"]}>August 12, 2016</div>
+                    </div>
+
+                    <div className={styles["post-text"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lectus
+                        massa. Pellentesque ornare mauris a auctor pellentesque. Sed sit amet
+                        metus at odio venenatis elemen mauris. Fusce mauris
+                        nibh, gravida eu eros sed, suscipit sollicitudin dui. Etiam tellus
+                        justo, fringilla at tempor in, mollis et felis.{" "}
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
