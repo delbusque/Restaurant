fe1e384312130702aa947367855735f4de019c23
Ventsislav Peychev <del.busque@gmail.com>
2023-04-18 16:57:58 +0300

    Delete unused controller functions

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -31,26 +31,6 @@ const getAllItems = async (req, res) => {
     res.status(200).json(items);
 }
 
-// const getItemByFamily = async (req, res) => {
-//     let items;
-//     let family;
-
-//     if (req.baseUrl == '/drinks') {
-//         items = await Item.find({ family: 'drink' });
-//         family = 'drinks';
-//     }
-//     if (req.baseUrl == '/food') {
-//         items = await Item.find({ family: 'food' });
-//         family = 'food';
-//     }
-
-//     if (items.length < 1) {
-//         return res.status(404).json({ mssg: `No ${family} there !` })
-//     }
-
-//     res.status(200).json(items);
-// }
-
 const getItemsByType = async (req, res) => {
     let family = req.baseUrl.split('/')[1];
 
