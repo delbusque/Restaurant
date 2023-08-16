f0df0b0e23d72fbc6ed9e191f721c8739d172024
delbusque <del.busque@gmail.com>
2023-08-16 11:47:33 +0300

    Add Send To Chef food button

--- frontend/src/components/Tables/Order.js
+++ frontend/src/components/Tables/Order.js
@@ -1,13 +1,27 @@
 import styles from './Order.module.css'
+import { useState } from 'react'
 
-const Order = ({ o, key, addItemHandler, deleteItemHandler }) => {
+
+const Order = ({ o, addItemHandler, deleteItemHandler }) => {
+    const [sendToChef, setSendToChef] = useState(0);
+
+    const toChefHandler = () => {
+        setSendToChef(o.count);
+    }
+
     return (
-        <div className={styles['tb-orders']} key={key}>
+        <div className={styles['tb-orders']}>
             <div className={styles['ord-name']}>{o.name}</div>
             <button className={styles['button-53']} role="button" onClick={() => deleteItemHandler(o)}>-</button>
             <button className={styles['button-53-green']} role="button" onClick={() => addItemHandler(o)}>+</button>
-            <div className={styles['ord-count']}><span className={styles['ord-counter']}>{o.count} </span> x {o.price.toFixed(2)}</div>
-            <div className={styles['ord-total']}>{(o.count * o.price).toFixed(2)} <span className={styles['lv']}>lv.</span></div>
+            <div className={styles['ord-count']}>
+                <span className={styles['ord-counter']}>{o.count} </span> x {o.price.toFixed(2)}
+            </div>
+
+            {o.family === 'food' && <button className={styles['button-53-blue']} role="button" onClick={toChefHandler}>{sendToChef} / {o.count}</button>}
+            <div className={styles['ord-total']}>{(o.count * o.price).toFixed(2)}
+                <span className={styles['lv']}>lv.</span>
+            </div>
         </div>
     )
 }
