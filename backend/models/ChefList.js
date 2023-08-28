8fe13cb17d62737c9a59fd4d2a5eed8deae1c6d0
Ventsislav Peychev <del.busque@gmail.com>
2023-08-28 14:59:23 +0300

    Update ChefList.js

--- backend/models/ChefList.js
+++ backend/models/ChefList.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const chefListSchema = new Schema({
-    orders: [],
-    // ready: [{ type: Schema.Types.ObjectId, ref: 'Item' }]
+    orders: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Item'
+    }],
+    ready: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Item'
+    }]
 })
 
 module.exports = mongoose.model('ChefList', chefListSchema)
