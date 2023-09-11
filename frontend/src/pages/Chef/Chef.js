ca000a7242968a38d0d3bcc21e9f36ab7b8b50a6
Ventsislav Peychev <del.busque@gmail.com>
2023-09-11 12:24:13 +0300

    Init Chef Order component

--- frontend/src/pages/Chef/Chef.js
+++ frontend/src/pages/Chef/Chef.js
@@ -1,10 +1,13 @@
+import ChefOrder from '../../components/Chef/ChefOrder';
 import styles from './Chef.module.css'
 
 const Chef = () => {
     return (
         <div className={styles["chef-orders"]}>
             <div>Ready Chef Orders</div>
-            <div className={styles["waiting"]}>Waiting Chef Orders</div>
+            <div className={styles["waiting"]}>
+                <ChefOrder />
+            </div>
         </div>
     )
 }
