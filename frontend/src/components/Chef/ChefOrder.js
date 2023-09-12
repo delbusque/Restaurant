7e043e34f6fec2e5931194d47517103c483ebc22
Ventsislav Peychev <del.busque@gmail.com>
2023-09-12 14:18:22 +0300

    Added neon shadow to ready button

--- frontend/src/components/Chef/ChefOrder.js
+++ frontend/src/components/Chef/ChefOrder.js
@@ -6,7 +6,6 @@ const ChefOrder = () => {
             <div className={styles['order-cont']}>
                 <div className={styles['order-info']}>
                     <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
                     <div className={styles['order-time']}>5 min</div>
                     <div>500 гр</div>
 
@@ -21,7 +20,6 @@ const ChefOrder = () => {
             <div className={styles['order-cont-1']}>
                 <div className={styles['order-info']}>
                     <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
                     <div className={styles['order-time']}>3 min</div>
                     <div>450 гр</div>
 
@@ -33,155 +31,6 @@ const ChefOrder = () => {
                 <button className={styles['order-ready-1']}>ГОТОВА</button>
             </div>
 
-            <div className={styles['order-cont-1']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
-                    <div className={styles['order-time']}>3 min</div>
-                    <div>450 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ПЪРЖЕНИ КАРТОФИ - СИРЕНЕ</div>
-                        <div className={styles['order-ingr']}>картофи, сирене</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready-1']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
-                    <div className={styles['order-time']}>5 min</div>
-                    <div>500 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ШОПСКА САЛАТА</div>
-                        <div className={styles['order-ingr']}>tomato, cucumber, onion, pepper, cheese</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
-                    <div className={styles['order-time']}>5 min</div>
-                    <div>500 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ШОПСКА САЛАТА</div>
-                        <div className={styles['order-ingr']}>tomato, cucumber, onion, pepper, cheese</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont-1']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
-                    <div className={styles['order-time']}>3 min</div>
-                    <div>450 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ПЪРЖЕНИ КАРТОФИ - СИРЕНЕ</div>
-                        <div className={styles['order-ingr']}>картофи, сирене</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready-1']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont-1']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
-                    <div className={styles['order-time']}>3 min</div>
-                    <div>450 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ПЪРЖЕНИ КАРТОФИ - СИРЕНЕ</div>
-                        <div className={styles['order-ingr']}>картофи, сирене</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready-1']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
-                    <div className={styles['order-time']}>5 min</div>
-                    <div>500 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ШОПСКА САЛАТА</div>
-                        <div className={styles['order-ingr']}>tomato, cucumber, onion, pepper, cheese</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
-                    <div className={styles['order-time']}>5 min</div>
-                    <div>500 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ШОПСКА САЛАТА</div>
-                        <div className={styles['order-ingr']}>tomato, cucumber, onion, pepper, cheese</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont-1']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
-                    <div className={styles['order-time']}>3 min</div>
-                    <div>450 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ПЪРЖЕНИ КАРТОФИ - СИРЕНЕ</div>
-                        <div className={styles['order-ingr']}>картофи, сирене</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready-1']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont-1']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>1</div>
-                    <div className={styles['order-number']}>#021</div>
-                    <div className={styles['order-time']}>3 min</div>
-                    <div>450 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ПЪРЖЕНИ КАРТОФИ - СИРЕНЕ</div>
-                        <div className={styles['order-ingr']}>картофи, сирене</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready-1']}>ГОТОВА</button>
-            </div>
-
-            <div className={styles['order-cont']}>
-                <div className={styles['order-info']}>
-                    <div className={styles['order-type']}>D</div>
-                    <div className={styles['order-number']}>#020</div>
-                    <div className={styles['order-time']}>5 min</div>
-                    <div>500 гр</div>
-
-                    <div>
-                        <div className={styles['order-name']}>ШОПСКА САЛАТА</div>
-                        <div className={styles['order-ingr']}>tomato, cucumber, onion, pepper, cheese</div>
-                    </div>
-                </div>
-                <button className={styles['order-ready']}>ГОТОВА</button>
-            </div>
         </>
     )
 }
