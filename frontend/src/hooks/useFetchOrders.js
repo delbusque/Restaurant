744bc2b468d7c8da7345127b99745ffcadeb3aea
delbusque <del.busque@gmail.com>
2023-09-19 14:33:49 +0300

    Update useFetchOrders.js

--- frontend/src/hooks/useFetchOrders.js
+++ frontend/src/hooks/useFetchOrders.js
@@ -6,7 +6,7 @@ const fetchOrders = () => axios.get('/chef/get-orders')
 export const useFetchOrders = () => {
     return useQuery('waiting-orders', fetchOrders,
         {
-            staleTime: 5000,
+            staleTime: 50000,
             select: data => data.data
         })
 }
