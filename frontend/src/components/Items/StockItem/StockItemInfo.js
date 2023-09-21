7d398231d602db5c5a6c01cf31008136a803982d
Ventsislav Peychev <del.busque@gmail.com>
2023-09-21 13:36:59 +0300

    Add ingredients to Item info

--- frontend/src/components/Items/StockItem/StockItemInfo.js
+++ frontend/src/components/Items/StockItem/StockItemInfo.js
@@ -6,10 +6,15 @@ const StockItemInfo = ({ item, setShowInfo }) => {
             <fieldset>
                 <button className={styles['modal-close']} onClick={() => setShowInfo(false)}> x </button>
                 <h2 className={styles['details-name']}>{item && item.name}</h2>
+                <div className={styles['details-ingr']}>{item?.ingredients.join(', ')}</div>
+
+                <div className={styles['details-price']}>{item && item.price.toFixed(2)} lv</div>
+                <div className={styles['details-quantity']}>{item && item.quantity.toFixed(3)} {item && item.quantityType}</div>
+
                 <div className={styles['details-family']}>{item && item.family}</div>
                 <div className={styles['details-type']}>{item && item.type}</div>
-                <div className={styles['details-quantity']}>{item && item.quantity.toFixed(3)} {item && item.quantityType}</div>
-                <div className={styles['details-price']}>{item && item.price.toFixed(2)} lv</div>
+
+
             </fieldset>
         </div>
     )
